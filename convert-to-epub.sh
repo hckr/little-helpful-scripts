6a1f76a1c15edb4ef9ea6eeec7240f2fe809f196
Luke <luke@luke.co.pl>
2016-03-07 20:42:44 +0100

    Correction in convert-to-epub.sh

--- convert-to-epub.sh
+++ convert-to-epub.sh
@@ -6,16 +6,14 @@ cd manuscript # directory with book chapters
 
 TITLE="Title of the book"
 AUTHOR="John Doe"
-COVER="![](images/title_page.png)" # could be used to insert cover image
+COVER="images/title_page.png"
 OUTFILE="book.epub"
 
 # where:
 # Book.txt in subsequent lines contains names of files (e.q. Markdown formatted) that should make up the book
 
-echo "$HEADER" | cat - `cat Book.txt` <<EOF | pandoc -o ../$OUTFILE
+echo "$HEADER" | cat - `cat Book.txt` <<EOF | pandoc -o ../$OUTFILE --epub-cover-image=$COVER
 % $TITLE
 % $AUTHOR
-
-$COVER
 
 EOF
